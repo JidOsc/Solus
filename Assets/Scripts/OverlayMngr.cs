dfa653af0ee4af4fca382e95c3bf88b5a0d0edfa
Philip Mattsson Thorin <fillefileyt@gmail.com>
2025-02-03 13:16:43 +0100

    staminabar

--- Assets/Scripts/OverlayMngr.cs
+++ Assets/Scripts/OverlayMngr.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class OverlayMngr : MonoBehaviour
 {
+    //OverlayMngr ska ha koll på allt i Overlay prefaben
     public GameObject player;
+    public GameObject health;
+    public GameObject stamina;
     [SerializeField] private Sprite[] images;
 
 
@@ -13,6 +16,11 @@ public class OverlayMngr : MonoBehaviour
 
     void Update()
     {
-        
+        PlayerMovement playerData = player.transform.Find("Capsule").GetComponent<PlayerMovement>();
+        HealthBar healthData = health.GetComponent<HealthBar>();
+        healthData.SetHealth((int)playerData.health);
+
+        StaminaBar staminaData = stamina.GetComponent<StaminaBar>();
+        staminaData.SetStamina((int)playerData.stamina);
     }
 }
