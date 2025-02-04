a001bf69ae5d9afe1042668c864d8045dc38564b
Viking7813 <lucas.jonsson@elev.ga.lbs.se>
2025-02-04 14:23:22 +0100

    Sandy boy

--- Assets/Scripts/RotationObject.cs
+++ Assets/Scripts/RotationObject.cs
@@ -7,25 +7,20 @@ public class RotationObject : MonoBehaviour
 
     private Vector3 player_pos = new Vector3(0, 0, 0);
 
-    //public GameObject[] objects;
     public GameObject player;
 
-   
     public void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-
-
-        //ob = GameObject.FindGameObjectsWithTag("Ore");
-        //objects = GameObject.FindGameObjectsWithTag("Enemies");
     }
+
     public void Update()
     {
         foreach (Transform tsfm in transform)
         {
             GameObject obj = tsfm.gameObject;
 
-            if (obj.tag == "Ore" || obj.tag == "Enemy")
+            if (obj.tag == "Ore" || obj.tag == "Enemies" || obj.tag == "DroppedOre")
             {
                 player_pos = player.transform.position;
                 player_pos.y = obj.transform.position.y;
