2889f24da01d2bbac1407a4d05f713b3a712d61d
Viking7813 <lucas.jonsson@elev.ga.lbs.se>
2025-02-04 10:53:18 +0100

    inte så sigma

--- Assets/Scripts/rotation_objects.cs
+++ Assets/Scripts/rotation_objects.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class rotation_objets : MonoBehaviour
@@ -5,13 +6,19 @@ public class rotation_objets : MonoBehaviour
 
     public GameObject player;
 
+    private Vector3 player_pos = new Vector3(0, 0, 0);
+
+    private Quaternion rotation; 
+
     private void Start()
     {
-        player.GetComponent<Transform>();
+
     }
 
     void Update()
     {
-        transform.LookAt(player.GetComponent<Transform>());
+        player_pos = player.transform.position;
+
+        transform.LookAt(new Vector3(player_pos.x, 0, player_pos.z));
     }
 }
