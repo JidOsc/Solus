106397fe60669690676d11fbfa9ca3a3fba8535c
Viking7813 <lucas.jonsson@elev.ga.lbs.se>
2025-02-06 13:58:23 +0100

    Spawnar fiender

--- Assets/Objectspawner.cs
+++ Assets/Objectspawner.cs
@@ -5,6 +5,8 @@ public class Objectspawner : MonoBehaviour
 {
     public GameObject level;
 
+    public GameObject enemy;
+
     public List<GameObject> stones = new List<GameObject>();
     public List<GameObject> ores = new List<GameObject>();
 
@@ -37,6 +39,20 @@ public class Objectspawner : MonoBehaviour
 
 
             var spawning = Instantiate(ores[0], pos, transform.rotation);
+            spawning.transform.parent = level.transform;
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            int x = Random.Range(-200, 200);
+            int z = Random.Range(-200, 200);
+            //float rotZ = Random.Range(0, 360);
+            Vector3 pos = new Vector3(x, 0, z);
+            //int hight = (int)Terrain.activeTerrain.terrainData.GetHeight(x, z) + 8;
+            pos.y = Terrain.activeTerrain.SampleHeight(pos) + Terrain.activeTerrain.GetPosition().y + 0.25f;
+
+
+            var spawning = Instantiate(enemy, pos, transform.rotation);
             spawning.transform.parent = level.transform;
         }
     }
