b91e1daa406e23d242372dfdb6c46d0c0d258cd7
Viking7813 <lucas.jonsson@elev.ga.lbs.se>
2025-02-12 10:09:46 +0100

    pushy push

--- Assets/Scripts/Player/Building.cs
+++ Assets/Scripts/Player/Building.cs
@@ -6,12 +6,15 @@ public class Building : MonoBehaviour
 
     public PlayerLook playerLook;
 
+    public GameObject stationPrefab;
+    public GameObject level;
+
     public void Start()
     {
-        
+        stationPrefab = GameObject.FindGameObjectWithTag("Station");
     }
 
-    public void build()
+    public void Build()
     {
         Vector3 mouse = Input.mousePosition;
 
@@ -20,7 +23,11 @@ public class Building : MonoBehaviour
 
         if(Physics.Raycast(casepoint, out hit, 10))
         {
-            
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                var build = Instantiate(stationPrefab, new Vector3(hit.point.x,hit.point.y, hit.point.z), transform.rotation);
+                build.transform.parent = level.transform;
+            }
         }
     }
 
